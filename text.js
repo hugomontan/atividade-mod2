11704d69007c830775414e3dbdff010fe993c24d
Hugo Montan <hugo.montan@sou.inteli.edu.br>
2025-05-14 15:04:08 -0300

    Create text.js

---
+++ text.js
@@ -0,0 +1,10 @@
+Aluno: Hugo Montan
+RA: A2025.1A.0174
+
+Explique com suas palavras o funcionamento do models, controller e fale sobre endpoints no projeto: 
+
+Dada minha breve experiência prévia com API’s, o conceito de Endpoint era atribuído em minha mente a rotas específicas para extração de dados via requisição. Dentro da lógica de Endpoints em WebDEV, Endpoints se referem a links (URLs), que respondem acionando ações. 
+
+Controllers funcionam como uma espécie de ‘middle office’ dentro de um sistema de web development, gerenciando a lógica entre o back (models) e o front (views). O controller, media a relação entre um endpoint, que viabiliza a ação de um usuário, com a execução de fato de tal ação, chamando uma função do models. 
+
+Models funciona como um fundamento base do sistema, gerenciando a estrutura dos dados, bem como sua manipulação durante operação.
